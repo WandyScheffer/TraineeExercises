034cf9bd51e12c3a92eacb75836002c5eb28925f
WandyRosa <wandy.s.scheffer.r@gmail.com>
2021-03-01 14:19:22 -0300

    fix a typo error

--- apiNodeClass/api/rotas/fornecedores/index.js
+++ apiNodeClass/api/rotas/fornecedores/index.js
@@ -1,7 +1,7 @@
 const roteador = require('express').Router();
 const TabelaFornecedor = require('./TabelaFornecedor')
 
-roteador.use('/', async (req, res) => {
+roteador.get('/', async (req, res) => {
     const resultados = await TabelaFornecedor.listar();
     res.send(JSON.stringify(resultados));
 })
