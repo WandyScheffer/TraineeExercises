4c1054dadafd9efa5cd443b10f729a256d8e02d0
WandyRosa <wandy.s.scheffer.r@gmail.com>
2021-03-03 05:07:04 -0300

    api exercise class 2 fix status code

--- myApiExercises/routes/gamesRoutes/index.js
+++ myApiExercises/routes/gamesRoutes/index.js
@@ -13,7 +13,7 @@ route.post('/', (req, res) => {
         const {name, platform} = req.body;
         if (!name || !platform) throw new Error("Está faltando algum dos parâmetros")
         games.push({name, platform});
-        res.status(204).send();
+        res.status(201).send();
     } catch (error) {
         console.log(error);
         res.send(JSON.stringify({message: error.message}))
