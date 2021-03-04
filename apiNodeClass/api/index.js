56d7b7e04cbd8bbfed1180d75c80ee7c0b7c4d25
WandyRosa <wandy.s.scheffer.r@gmail.com>
2021-03-04 13:54:14 -0300

    added serializer to control content

--- apiNodeClass/api/index.js
+++ apiNodeClass/api/index.js
@@ -8,6 +8,7 @@ const route_fornecedores = require('./rotas/fornecedores');
 const NaoEncontrado = require('./erros/NaoEncontrado');
 const CampoInvalido = require('./erros/CampoInvalido');
 const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos');
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado');
 
 app.use(bodyParser.json());
 
@@ -20,6 +21,8 @@ app.use((erro, req, res, next) => {
         status = 404;
     }else if(erro instanceof CampoInvalido || erro instanceof DadosNaoFornecidos){
         status = 400;
+    }else if(erro instanceof ValorNaoSuportado){
+        status = 406;
     }
     res.status(status).send(JSON.stringify({message: erro.message, id: erro.idErro}))
 })
