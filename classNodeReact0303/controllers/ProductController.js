2445d766eee128be8664faeda4a77080ccdc436a
WandyRosa <wandy.s.scheffer.r@gmail.com>
2021-03-22 13:00:13 -0300

    fixed 'select all' from 'products', using sequelize features

--- classNodeReact0303/controllers/ProductController.js
+++ classNodeReact0303/controllers/ProductController.js
@@ -7,19 +7,17 @@ const Product = {
         try {
             const limit = Number(req.query.limit) || 10;
             const offset = Number(req.query.offset) || 0;
-            const result = await db.Products.findAll({include: db.Categorys, limit, offset});
-            const relevant_data = result.map( product => {
-                return {
-                    id: product.id,
-                    name: product.name,
-                    price: product.price,
-                    category_id: product.category_id,
-                    category: product.Category.name,
-                    status: product.Category.status
-                };
-            })
+            const result = await db.Products.findAll({
+                include: {
+                    model: db.Categorys,
+                    attributes: ['name',['status', 'category_status']]
+                }, 
+                limit, 
+                offset,
+                attributes: ['id','name','price','category_id'],            
+            });
             
-            return res.status(200).send(serializator.serializing(relevant_data, res.getHeader('Content-Type')));
+            return res.status(200).send(serializator.serializing(result, res.getHeader('Content-Type')));
         } catch (error) {
             return res.status(500).send(serializator.serializing({ message: error.message }, res.getHeader('Content-Type')));
         }
